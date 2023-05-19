84452f0b724a013c4ddf829b3920be265d3ddbdd
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-05-19 08:59:17 -0300

    Creating a Class AppDbContext

---
+++ LanchesMac/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
