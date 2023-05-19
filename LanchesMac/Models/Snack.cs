18f58ed13151382dc2be9df1be1413fad0810e0b
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-05-19 08:32:17 -0300

    creating the domain model

---
+++ LanchesMac/Models/Snack.cs
@@ -0,0 +1,20 @@
+﻿namespace LanchesMac.Models
+{
+    public class Snack
+    {
+        public int SnackId { get; set; }
+        public string SnackName { get; set;}
+        public string ShortDescription { get; set;}
+        public string DetailDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsFavoriteSnack { get; set; }
+        public bool InStock { get; set; }
+
+        //foreign key
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
