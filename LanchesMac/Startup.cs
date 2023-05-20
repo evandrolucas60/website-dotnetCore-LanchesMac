b913f2326d33de1a0ab5cd1731abfc73e10f9062
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-05-20 08:38:25 -0300

    Updating connection string to be able for entityframeworkcore 7.0

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -14,7 +14,8 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         services.AddControllersWithViews();
     }
 
