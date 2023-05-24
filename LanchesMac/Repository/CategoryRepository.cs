e8c6526c80271c4bf45d7e7aa380cdc9606bbcbd
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-05-24 09:43:47 -0300

    Implementing the Repository pattern for Category

---
+++ LanchesMac/Repository/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repository.Interfaces;
+
+namespace LanchesMac.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Category> Categories => _context.Categories;
+
+    }
+}
---
+++ LanchesMac/Repository/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repository.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> Categories { get; }
+    }
+}
