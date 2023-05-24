d85e886dd4b0bfe5dd24f8b3fba2b1cda3122517
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-05-24 10:03:40 -0300

    Implementing the Repository pattern for Snacks

---
+++ LanchesMac/Repository/Interfaces/ISnackRepository.cs
@@ -0,0 +1,12 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repository.Interfaces
+{
+    public interface ISnackRepository
+    {
+        IEnumerable<Snack> Snacks { get; }
+        IEnumerable<Snack> FavoriteSnacks { get; }
+        Snack GetSnackById(int id);
+
+    }
+}
---
+++ LanchesMac/Repository/SnackRepository.cs
@@ -0,0 +1,29 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repository
+{
+    public class SnackRepository : ISnackRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SnackRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Snack> Snacks => _context.Snacks
+            .Include(c => c.Category);
+
+        public IEnumerable<Snack> FavoriteSnacks => _context.Snacks
+            .Where(s => s.IsFavoriteSnack)
+            .Include(c => c.Category);
+
+        public Snack GetSnackById(int id)
+        {
+            return _context.Snacks.FirstOrDefault(s => s.SnackId == id);
+        }
+    }
+}
