4f64d0472c03f4c8a19e4e8afd1ca1d931e10919
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-05-25 07:06:58 -0300

    Creating the Controller a View to display the snacks

---
+++ LanchesMac/Controllers/SnackController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class SnackController : Controller
+    {
+        private readonly ISnackRepository _snackRepository;
+
+        public SnackController(ISnackRepository snackRepository)
+        {
+            _snackRepository = snackRepository;
+        }
+
+        public IActionResult List()
+        {
+            var snacks = _snackRepository.Snacks;
+            return View(snacks);
+        }
+    }
+}
