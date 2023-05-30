7eda3f51c990b071ed86dcd609bd04a04ae8b549
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-05-30 10:04:42 -0300

    Completing the Shopping Cart - including important methods (GetCartItens, ClearCart, GetCartTotal)

--- LanchesMac/Models/Cart.cs
+++ LanchesMac/Models/Cart.cs
@@ -1,4 +1,6 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Migrations;
+using Microsoft.EntityFrameworkCore;
 
 namespace LanchesMac.Models
 {
@@ -24,7 +26,7 @@ namespace LanchesMac.Models
             var context = services.GetService<AppDbContext>();
 
             //obtém ou gera o Id do carrinho
-            string cartId = session.GetString("CartId")??Guid.NewGuid().ToString();
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
 
             //atribui o id do carrinho na sessão
             session.SetString("CartId", cartId);
@@ -39,7 +41,7 @@ namespace LanchesMac.Models
         public void AddToCart(Snack snack)
         {
             var cartItem = _context.CartItems.SingleOrDefault(
-                s=> s.Snack.SnackId == snack.SnackId &&
+                s => s.Snack.SnackId == snack.SnackId &&
                 s.CartId == CartId);
 
             if (cartItem == null)
@@ -66,11 +68,11 @@ namespace LanchesMac.Models
                 s => s.Snack.SnackId == snack.SnackId &&
                 s.CartId == CartId);
 
-            var localQuantity =  0;
+            var localQuantity = 0;
 
             if (cartItem != null)
             {
-                if (cartItem.Quantity > 1) 
+                if (cartItem.Quantity > 1)
                 {
                     cartItem.Quantity--;
                     localQuantity = cartItem.Quantity;
@@ -83,6 +85,38 @@ namespace LanchesMac.Models
 
             _context.SaveChanges();
             return localQuantity;
+        }
+
+
+        public List<CartItem> GetCartItens()
+        {
+            return CartItems ??
+                   (CartItems =
+                       _context.CartItems
+                       .Where(c => c.CartId == CartId)
+                       .Include(c => c.Snack)
+                       .ToList());
+        }
+
+
+        public void ClearCart()
+        {
+            var cartItem = _context.CartItems
+                .Where(c => c.CartId == CartId);
+
+            //remove todas as entidade do carrinho de compra
+            _context.CartItems.RemoveRange(cartItem);
+            _context.SaveChanges();
+        }
+
+
+        public decimal GetCartTotal()
+        {
+            var total = _context.CartItems
+                .Where(c => c.CartId == CartId)
+                .Select(c => c.Snack.Price * c.Quantity).Sum();
+
+            return total;
         }
     }
 }
