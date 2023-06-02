164ca10284fa0cd9dc13b93d9c01568b5695cc05
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-06-02 09:25:42 -0300

    Displaying favorite snacks

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using LanchesMac.Models;
+using LanchesMac.Repository.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,9 +8,20 @@ namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ISnackRepository _snackRepository;
+
+        public HomeController(ISnackRepository snackRepository)
+        {
+            _snackRepository = snackRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                FavoriteSnacks = _snackRepository.FavoriteSnacks
+            };
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
