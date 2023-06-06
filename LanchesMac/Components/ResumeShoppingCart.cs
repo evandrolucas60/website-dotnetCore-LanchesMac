834cb0c4e6c5ff27271a6999111b5a39a928b03f
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-06-06 10:35:24 -0300

    Creating a ViewComponent to display cart details

---
+++ LanchesMac/Components/ResumeShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using LanchesMac.Models;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Components
+{
+    public class ResumeShoppingCart : ViewComponent
+    {
+        private readonly Cart _cart;
+
+        public ResumeShoppingCart(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _cart.GetCartItens();
+
+            _cart.CartItems = itens;
+
+            var cartVM = new CartViewModel
+            {
+                Cart = _cart,
+                CartTotal = _cart.GetCartTotal()
+            };
+
+            return View(cartVM);
+        }
+    }
+}
