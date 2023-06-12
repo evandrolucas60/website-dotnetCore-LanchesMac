9f89d7ede1d3344f0976fc8e51d2abd406d1e95b
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-06-12 08:15:05 -0300

    Creating a TagHelper

---
+++ LanchesMac/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace LanchesMac.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Address { get; set; }
+        public string EmailContent { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Address);
+            output.Content.SetContent(EmailContent);
+        }
+    }
+}
