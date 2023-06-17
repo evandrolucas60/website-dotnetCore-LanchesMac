21ea01a9a55d5d0a4f30d1c24f13e14f0419c8ac
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-06-17 09:46:32 -0300

    Creating the RequestController controller

---
+++ LanchesMac/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly Cart _cart;
+
+        public OrderController(IOrderRepository orderRepository, Cart cart)
+        {
+            _orderRepository = orderRepository;
+            _cart = cart;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            return View();
+        }
+
+    }
+
+}
