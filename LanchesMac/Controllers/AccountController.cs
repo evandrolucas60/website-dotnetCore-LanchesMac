74eaa0a59366fdc93fe66f449bdc0235f8c6e240
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-06-23 15:19:17 -0300

    Implementing the Register

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -50,5 +50,31 @@ namespace LanchesMac.Controllers
             ModelState.AddModelError("", "Falha ao realizar o login!!");
             return View(loginVM);
         }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(LoginViewModel registerVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registerVM.UserName };
+                var result = await _userManager.CreateAsync(user, registerVM.PassWord);
+
+                if (result.Succeeded) 
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Register", "Falha ao registrar o usuário");
+                }
+            }
+        }
+
     }
 }
