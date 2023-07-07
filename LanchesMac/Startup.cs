e0d4ea4e0fab8776908af205e2a6e76dd7f43834
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-07-07 09:45:49 -0300

    Implementation - Configuration and creation of infrastructure files

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -44,6 +44,8 @@ public class Startup
                 });
         });
 
+        services.Configure<ConfigurationImages>(Configuration.GetSection("ProductsImagesFolderName"));
+
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped(sp => Cart.GetCart(sp));
 
