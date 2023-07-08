1dd3b6d6a425ac91c476bee10a275b4c72ac6ad5
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-07-08 10:20:24 -0300

    Implementation - Displaying the options for Uploading and Querying images

---
+++ LanchesMac/Areas/Admin/Controllers/AdminImagesController.cs
@@ -0,0 +1,26 @@
+﻿using LanchesMac.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area ("Admin")]
+    [Authorize(Roles ="Admin")]
+    public class AdminImagesController : Controller
+    {
+        private readonly ConfigurationImages _myconfig;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AdminImagesController(IOptions<ConfigurationImages> myconfig, IWebHostEnvironment webHostEnvironment)
+        {
+            _myconfig = myconfig.Value;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
