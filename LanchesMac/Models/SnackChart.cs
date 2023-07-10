be278ce947a53bd22d14f510ff04a90e0e27bcc3
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-07-10 20:11:01 -0300

    Generating charts using the GoogleChart library - Creating the domain model

---
+++ LanchesMac/Models/SnackChart.cs
@@ -0,0 +1,9 @@
+﻿namespace LanchesMac.Models
+{
+    public class SnackChart
+    {
+        public string SnackName { get; set; }
+        public int SnackQuantity { get; set; }
+        public decimal SnackTotalValue { get; set; }
+    }
+}
